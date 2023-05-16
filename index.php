c05988f9ce80cfaa52c6b92537c2a94af735a22c
RiccardoGrande <grandericcardo1997@gmail.com>
2023-05-16 16:11:10 +0200

    add movie and tv series

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ class Movie
     public $description;
     public $genre;
 
-    public static $type = 'Best Film';
+    public static $type = 'My top last watched Film';
 
     public function __construct(string $title, string $description, string $genre)
     {
@@ -33,3 +33,36 @@ class Movie
     }
 
 }
+
+class TvSeries
+{
+
+    public $title;
+    public $description;
+    public $genre;
+
+    public static $type = 'My top last watched TV Series';
+
+    public function __construct(string $title, string $description, string $genre)
+    {
+        $this->title = $title;
+        $this->description = $description;
+        $this->genre = $genre;
+
+    }
+
+    public function getType()
+    {
+        # code...
+        return self::$type; // human
+    }
+
+}
+
+$movie = new Movie('Ocean\'s Eleven ', 'Ocean\'s Eleven is an American caper film. It is directed by Steven Soderbergh and produced by the late Jerry Weintraub. A reboot of the 1960 film.', 'Crime, Thriller');
+
+$tvseries = new TvSeries('Supernatural', 'Supernatural is an American dark fantasy drama television series created by Eric Kripke.', 'Horror, Fantasy, Adventure, Drama, Thriller');
+
+var_dump($movie);
+
+var_dump($tvseries);
